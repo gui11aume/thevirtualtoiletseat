2f9b9bb5806c7abb5cab9e5047537d2658410636
Guillaume Filion <guillaume.filion@gmail.com>
2011-12-23 16:54:19 +0100

    Increased security. Check uid upon POST, keep uid upon reload.

--- thevirtualtoiletseat.py
+++ thevirtualtoiletseat.py
@@ -19,6 +19,36 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 # ------------------- Local constants ------------------- #
 DIRTY = 'The virtual toilet seat is dirty.'
 CLEAN = 'The virtual toilet seat is clean.'
+# ------------------------------------------------------- #
+
+
+def identify_from_cookie(request_handler):
+   """Return the 'uid' attribute from cookie if properly defined and
+   in the data-base (up-to-date), or False otherwise."""
+
+   try:
+      return app_admin.User.gql(
+                   'WHERE ANCESTOR IS :1 AND uid = :2',
+                   app_admin.user_key(),
+                   request_handler.request.cookies.get('uid')
+             )[0].uid
+   except (KeyError, IndexError):
+      # No cookie or no longer in the data-base, respectively.
+      return False
+
+def user_is_inside(uid):
+   """Return 'True' if user has earliest 'first_ping' attribute and
+   is still up-to-date, False otherwise."""
+
+   try:
+      return app_admin.User.gql(
+                   'WHERE ANCESTOR IS :1 ' \
+                 + 'ORDER BY first_ping ASC LIMIT 1',
+                    app_admin.user_key()
+             )[0].uid == uid
+   except IndexError:
+      return False
+
 
 class QueueHandler(webapp.RequestHandler):
    """You are technically on the outside. You'll get in only upon
@@ -34,10 +64,13 @@ class QueueHandler(webapp.RequestHandler):
       if there_is_no_toilet_seat:
          app_admin.create_toilet_seat()
 
-      # Give user an ID (gets lost upon reaload).
-      uid = str(random.random())
-      app_admin.create_user(uid)
-      self.response.headers.add_header('Set-Cookie', 'uid=%s' % uid)
+      # Does user already exist (has a cookie up-to-date)?
+      uid = identify_from_cookie(self)
+      if not uid:
+         # Give user a cookie ID.
+         uid = str(random.random())
+         app_admin.create_user(uid)
+         self.response.headers.add_header('Set-Cookie', 'uid=%s' % uid)
 
       dot = os.path.dirname(__file__)
 
@@ -80,18 +113,10 @@ class PingHandler(webapp.RequestHandler):
                             )
       db.delete(ditched_users_query)
  
-      # Now get first user in line.
-      first_user_query = app_admin.User.gql(
-                              'WHERE ANCESTOR IS :1 ' \
-                            + 'ORDER BY first_ping ASC LIMIT 1',
-                               app_admin.user_key()
-                         )
-
-      user_is_first = uid == first_user_query[0].uid
 
       dot = os.path.dirname(__file__)
 
-      if user_is_first:
+      if user_is_inside(uid):
          # Bingo! You've been lucky or patient. Welcome inside.
          toilet_query = app_admin.ToiletSeat.gql(
                               'WHERE ANCESTOR IS :1 ',
@@ -118,11 +143,17 @@ class ToiletUpdateHandler(webapp.RequestHandler):
    """Handle user action."""
 
    def post(self):
-      query_result = app_admin.ToiletSeat.gql(
-                 'WHERE ANCESTOR IS :1',
-                 app_admin.toilet_key()
-             )
-      toilet_data = query_result[0]
+      # Check that user is inside.
+      uid = identify_from_cookie(self)
+      # Early return if this POST is forbidden.
+      if not user_is_inside(uid):
+         return
+
+      toilet_query = app_admin.ToiletSeat.gql(
+                           'WHERE ANCESTOR IS :1 ',
+                            app_admin.toilet_key()
+                      )
+      toilet_data = toilet_query[0]
       user_action = self.request.get('action')
       
       if user_action in ('pee', 'poo'):
